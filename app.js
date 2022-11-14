6f446f9f9648a30d2ca4a000a5b778a276d9a2a1
Roman Chuchev <romanok20@gmail.com>
2022-11-14 18:25:27 +0300

    feat: chanhe styles

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 function slidesPlugin(activeSlide = 2) {
-    const slides = document.querySelectorAll('.slide')
-    slides[activeSlide].classList.add('active')
-
+    const slides = document.querySelectorAll(".slide");
+    slides[activeSlide].classList.add("active");
 
     for (const slide of slides) {
-        slide.addEventListener('click', () => {
-            clearActiveClasses()
-            slide.classList.add('active')
-        })
+        slide.addEventListener("click", () => {
+            clearActiveClasses();
+            slide.classList.add("active");
+        });
     }
 
     function clearActiveClasses() {
         slides.forEach((slide) => {
-            slide.classList.remove('active')
-        })
+            slide.classList.remove("active");
+        });
     }
-
 }
-slidesPlugin(2)
+slidesPlugin(2);
